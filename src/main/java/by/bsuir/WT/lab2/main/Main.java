9c8dba8a7c5b9d7c3b331712c74162db91354ed3
masha-mcr <masha1747dk@gmail.com>
2021-11-12 01:37:01 +0300

    fix: removed duplicates from xml file

--- src/main/java/by/bsuir/WT/lab2/main/Main.java
+++ src/main/java/by/bsuir/WT/lab2/main/Main.java
@@ -16,13 +16,21 @@ public class Main {
 
         Criteria kettleCriteria = new Criteria(SearchCriteria.Kettle.class.getSimpleName());
         List<Appliance> kettles = service.find(kettleCriteria);
-        List<Appliance> kettlesOrderedByName = service.sort(Comparator.comparing(Appliance::getName), kettles);
-        System.out.println("\nВсе чайники(в алфавитном порядке): ");
-        PrintApplianceInfo.print(kettlesOrderedByName);
+        if (kettles !=null) {
+            List<Appliance> kettlesOrderedByName = service.sort(Comparator.comparing(Appliance::getName), kettles);
+            System.out.println("\nВсе чайники(в алфавитном порядке): ");
+            PrintApplianceInfo.print(kettlesOrderedByName);
+        }
 
         List<Appliance> minPriceAppliances = service.getMin(Comparator.comparing(Appliance::getPrice));
-        System.out.println("\nСамый дешевый прибор: ");
-        PrintApplianceInfo.print(minPriceAppliances);
+        if (minPriceAppliances != null) {
+            System.out.println("\nСамый дешевый прибор: ");
+            PrintApplianceInfo.print(minPriceAppliances);
+        }
 
+        Criteria ovenCriteria = new Criteria(SearchCriteria.Oven.class.getSimpleName());
+        ovenCriteria.add("powerConsumption", 2000);
+        List<Appliance> ovens = service.find(ovenCriteria);
+        PrintApplianceInfo.print(ovens);
     }
 }
